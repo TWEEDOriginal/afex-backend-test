3c7e794f67373c780db81f6ced36d89737df5664
TWEEDOriginal <oguntadetweed1@gmail.com>
2022-02-09 14:38:00 +0100

    feat: docker configurations

--- devtest/settings/production.py
+++ devtest/settings/production.py
@@ -2,14 +2,3 @@ from .staging import *
 
 # Override staging settings here
 ALLOWED_HOSTS.append("afex-backend-test.herokuapp.com")
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
--- devtest/settings/staging.py
+++ devtest/settings/staging.py
@@ -20,3 +20,14 @@ DATABASES = {
         "PORT": DB_PORT,
     }
 }
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
